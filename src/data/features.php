d6584b9352c9115445b59b7f0d4d55bee1e5c5ef
Niyas C <niyasc@localhost.localdomain>
2014-03-24 00:20:42 +0530

    Started priliminary structure of random subset sampling

--- src/data/features.php
+++ src/data/features.php
@@ -12,6 +12,7 @@
 $features = array(
   array('uid' => '100000184531749','extraversion' => '2.5','agreeableness' => '2.77778','conscientiousness' => '2.77778','neuroticism' => '2.5','openness' => '1.8'),
   array('uid' => '100000966526483','extraversion' => '4.625','agreeableness' => '4.33333','conscientiousness' => '4','neuroticism' => '2.625','openness' => '4.5'),
+  array('uid' => '100000807245444','extraversion' => '2.375','agreeableness' => '3.55556','conscientiousness' => '2.66667','neuroticism' => '4','openness' => '3.6'),
   array('uid' => '1360550593','extraversion' => '3.5','agreeableness' => '3.66667','conscientiousness' => '2','neuroticism' => '2.25','openness' => '4.3'),
   array('uid' => '100001108193506','extraversion' => '2','agreeableness' => '4.33333','conscientiousness' => '2.66667','neuroticism' => '3','openness' => '3.3'),
   array('uid' => '100006599203697','extraversion' => '1.875','agreeableness' => '4.22222','conscientiousness' => '3','neuroticism' => '4.125','openness' => '3.3'),
@@ -20,7 +21,9 @@ $features = array(
   array('uid' => '100002083630229','extraversion' => '3.25','agreeableness' => '3.88889','conscientiousness' => '2.77778','neuroticism' => '2.125','openness' => '4.2'),
   array('uid' => '100003363377993','extraversion' => '3.625','agreeableness' => '3.22222','conscientiousness' => '3.77778','neuroticism' => '2.75','openness' => '4'),
   array('uid' => '100000745663429','extraversion' => '4','agreeableness' => '3.77778','conscientiousness' => '3.44444','neuroticism' => '2.25','openness' => '3.7'),
-  array('uid' => '100003470353128','extraversion' => '3.75','agreeableness' => '3.77778','conscientiousness' => '2.88889','neuroticism' => '2.375','openness' => '3.8')
+  array('uid' => '100003470353128','extraversion' => '3.75','agreeableness' => '3.77778','conscientiousness' => '2.88889','neuroticism' => '2.375','openness' => '3.8'),
+  array('uid' => '100003271962779','extraversion' => '3.875','agreeableness' => '4.22222','conscientiousness' => '3.88889','neuroticism' => '1.625','openness' => '3.3'),
+  array('uid' => '100001324430179','extraversion' => '2.375','agreeableness' => '3.88889','conscientiousness' => '3','neuroticism' => '2.625','openness' => '4.2')
 );
 
 $list=["uid","extraversion","agreeableness","conscientiousness","neuroticism","openness"];
