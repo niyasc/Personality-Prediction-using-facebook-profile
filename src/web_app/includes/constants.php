2aa2f6dc2485aeed78c0d4af723ec933ea646d48
root <root@engineerinme.com>
2014-04-09 12:47:18 -0400

    Credentials changed

--- src/web_app/includes/constants.php
+++ src/web_app/includes/constants.php
@@ -6,16 +6,14 @@
      **********************************************************************/
 
     // your database's name
-    define("DATABASE", "technoli_mydb");
-
-    // your database's password
-    define("PASSWORD", "9p6ln^*0zIQ8g|S");
+    define("DATABASE", "fb_app");
+    define("PASSWORD", "kgggdkp1992");
 
     // your database's server
     define("SERVER", "localhost");
 
     // your database's username
-    define("USERNAME", "technoli");
+    define("USERNAME", "root");
     
 
 ?>
